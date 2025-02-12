4738eab4c87dd1b1fdb73c1d9387ed570682be97
ElvisMAM05 <eaguilar119500862@gmail.com>
2025-02-12 10:27:23 -0600

    Javasc

---
+++ js/Gato.js
@@ -0,0 +1,19 @@
+const espacio0=document.getElementById("espacio0")
+const espacio1=document.getElementById("espacio1")
+const espacio2=document.getElementById("espacio2")
+const espacio3=document.getElementById("espacio3")
+const espacio4=document.getElementById("espacio4")
+const espacio5=document.getElementById("espacio5")
+const espacio6=document.getElementById("espacio6")
+const espacio7=document.getElementById("espacio7")
+const espacio8=document.getElementById("espacio8")
+
+
+espacio0.addEventListener("click",()=>{
+
+
+
+
+})
+
+
