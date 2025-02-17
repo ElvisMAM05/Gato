bbd4fc3c5bb31b27ad9200a987bfecf0803527e1
ElvisMAM05 <eaguilar119500862@gmail.com>
2025-02-17 15:10:49 -0600

    Pausa

--- js/Gatovsmachine.js
+++ js/Gatovsmachine.js
@@ -11,7 +11,7 @@ let turnm=false
 let turnp=true
 let turnP= document.getElementById("turnP")
 let turnM= document.getElementById("turnM")
-
+let resetB=document.getElementById("reset")
 
 human.textContent = `Player's Wins: ${winX}`;
 machine.textContent = `Machine's Wins: ${winO}`
@@ -43,9 +43,7 @@ function humanMove(){
     });
 }
 
-
 humanMove()
-
 
 function movimientoMaquina(){
     if(!gameOver){
@@ -58,7 +56,7 @@ function movimientoMaquina(){
         turnM.textContent= "player's turn"
         Turns.appendChild(turnM)
         const move= Math.floor(Math.random()*spaceO.length)
-    spaceO[move].innerHTML="O"
+            spaceO[move].innerHTML="O"
                 checkline(0,1,2)
                 checkline(1,4,7)
                 checkline(6,4,2)
@@ -67,67 +65,43 @@ function movimientoMaquina(){
                 checkline(6,7,8)
                 checkline(0,3,6)
                 checkline(2,5,8)
-
-
-
     }
-
 }
 }
-
-function sendO(){
-    const buttonS = document.createElement("button");
-    buttonS.textContent = "Send";
-    buttonS.setAttribute("class", "Reset");
-    ContainerP.appendChild(buttonS);
-    buttonS.addEventListener("click", () =>{
-    winO+=1;
-    localStorage.setItem("winO", winO); // Guardar el puntaje actualizado en localStorage
-    window.location.reload();
-})
-}
-function sendX() {
-    const buttonS = document.createElement("button");
-    buttonS.textContent = "Send";
-    buttonS.setAttribute("class", "Reset");
-    ContainerP.appendChild(buttonS);
-    buttonS.addEventListener("click", () => {
-        winX += 1;
-        let marc=document.createElement("p")
-        marc.textContent= winX
-        human.appendChild(marc)
-
-        
-        localStorage.setItem("winX", winX); // Guardar el puntaje actualizado en localStorage
-        window.location.reload();
-    });
-}
-
-
 
 function Winners(c1,c2,c3){
 if( gatos[c1].innerHTML==="X" &&
     gatos[c2].innerHTML==="X" &&
     gatos[c3].innerHTML==="X"){
+        turnM.remove()
+        turnP.remove()
+        gatos[c1].setAttribute("class", "cc1")
+        gatos[c2].setAttribute("class", "cc2")
+        gatos[c3].setAttribute("class", "cc3")
+        
     let winner= document.createElement("p")
     winner.textContent="Player's win"
-    ContainerP.appendChild(winner)
+    Turns.appendChild(winner)
     gameOver=true //Indica que el juego terminó//
-    sendX()
-
+    
 }else{
     if( gatos[c1].innerHTML==="O" &&
         gatos[c2].innerHTML==="O" &&
         gatos[c3].innerHTML==="O"){
+            turnM.remove()
+            turnP.remove()
+            gatos[c1].setAttribute("class", "cc1")
+            gatos[c2].setAttribute("class", "cc2")
+            gatos[c3].setAttribute("class", "cc3")
+        
         let winner= document.createElement("p")
-        winner.textContent="The winner is the machine"
-        ContainerP.appendChild(winner)
-        gameOver=true //Indica que el juego terminó//
-        sendO() 
+        winner.textContent="machine's win"
+        Turns.appendChild(winner)
+    
 
+        gameOver=true //Indica que el juego terminó//
     }
 }
-
 }
 function checkline(c1, c2, c3){
     if(gatos[c1].innerHTML.length >0 && 
@@ -136,19 +110,25 @@ function checkline(c1, c2, c3){
                 {
                     Winners(c1,c2,c3)
                     Turns.setAttribute("class","anim Turns ")
+                    if (gatos[c1].textContent === "X") {
+                        winX += 1;
+                        localStorage.setItem("winX", winX); // Guardar el puntaje actualizado en localStorage
+                        human.textContent= `Player's Wins: ${winX}`;
+                    }else{
+                        if(gatos[c1].textContent==="O"){
+                            winO+=1;
+                            localStorage.setItem("winO", winO); // Guardar el puntaje actualizado en localStorage
+                            machine.textContent = `Machine's Wins: ${winO}`
+
+                        }
+                    }
                 }
 }
 
-function markerM(){
-
+function reset(){
+    resetB.addEventListener("click", ()=>{
+        window.location.reload()
+    })
 }
 
-
-
-markerM()
-
-
-
-
-
-
+reset()
